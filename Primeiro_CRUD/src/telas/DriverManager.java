3fa1f2cb00f09cdafab6de416fc2091a9319f384
Lucas de Lima Nascimento <118392799+LucasLNascimento@users.noreply.github.com>
2023-05-08 21:34:38 -0300

    Add files via upload

---
+++ Primeiro_CRUD/src/telas/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package telas;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author aluno
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3308db_escola, String mykaelteste, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
